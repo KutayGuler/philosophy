9d752f934ec957b5bae832c3351081f0c3c34028
Kutay Güler <75138362+KutayGuler@users.noreply.github.com>
2021-06-19 15:20:24 +0300

    Update slavery.test.js

--- slavery/slavery.test.js
+++ slavery/slavery.test.js
@@ -1,5 +1,5 @@
 const enslavement = require('./slavery');
 
-test('irrationality of slavery', () => {
+test("irrationality of slavery and master's self imprisonment", () => {
   expect(enslavement()).toBe(false);
 })
